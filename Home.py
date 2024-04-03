31866816b252365025c37275af28905db230cf3d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-04-03 16:49:45 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
 
   gdf_polygon = gpd.read_file(uploaded_file)
-  # gdf_polygon = gdf_polygon.to_crs({'init': 'epsg:32633'})
+  gdf_polygon = gdf_polygon.to_crs({'init': 'epsg:32633'})
   gdf_polygon['Oppervlakte (Km2)'] = gdf_polygon['geometry'].map(lambda x: round(x.area / 10**6))
   gdf_polygon = gdf_polygon.to_crs({'init': 'epsg:4326'})
 
@@ -53,11 +53,10 @@ INITIAL_VIEW_STATE = pdk.ViewState(
 
 tooltip = {
     "text": "{naam} \nOppervlakte (Km2): {Oppervlakte (Km2)} \nAanvullende informatie: {extras}",
-    # "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
 }
 
 
-r = pdk.Deck(layers=[layers],initial_view_state=INITIAL_VIEW_STATE,map_style=pdk.map_styles.LIGHT,tooltip=tooltip,)
+r = pdk.Deck(layers=[layers],initial_view_state=INITIAL_VIEW_STATE,map_style=pdk.map_styles.LIGHT,tooltip=tooltip)
 
 st.pydeck_chart(r)
   
