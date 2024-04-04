80180f2aa1a8cc19c558de554eb19b63e59597fe
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-04-04 11:08:04 +0200

    Update drawing.py

--- pages/drawing.py
+++ pages/drawing.py
@@ -9,7 +9,9 @@ import folium
 from folium.plugins import Draw, Fullscreen, LocateControl
 from streamlit_folium import st_folium
 
-REGIONAL_NAME = st.selectbox("Choose a location", ('Terschelling','Amsterdam' ))
+REGIONAL_NAME  = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
 
 region = ox.geocoder.geocode_to_gdf(REGIONAL_NAME)
 buildings = ox.geometries.geometries_from_polygon(region['geometry'][0], tags = {'building': True})
