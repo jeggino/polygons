9356ae5daba1dbea30d6b6ef9d5246bc4224f1d8
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-04-04 10:46:16 +0200

    Update drawing.py

--- pages/drawing.py
+++ pages/drawing.py
@@ -18,6 +18,32 @@ tost = buildings[buildings.index.isin(['way'], level=0)].iloc[:,:1]
 tost= tost.to_crs({'init': 'epsg:32633'})
 tost['Oppervlakte (m2)'] = tost['geometry'].map(lambda x: round(x.area))
 
+# create exagons
+from h3 import h3
+from shapely.geometry import Polygon
+
+h3_level = 10 # ~6m2 area resolution
+ 
+def lat_lng_to_h3(row):
+    return h3.geo_to_h3(row.centroid.y, row.centroid.x, h3_level)
+
+def add_geometry(row):
+    points = h3.h3_to_geo_boundary(
+      row['h3'], True)
+    return Polygon(points)
+
+
+gdf_centroids = buildings[buildings.index.isin(['way'], level=0)].iloc[:,:1]
+gdf_centroids["centroid"] = gdf_centroids["geometry"].centroid
+gdf_centroids = gdf_centroids.set_geometry("centroid")
+gdf_centroids.reset_index(drop=True,inplace=True)
+
+gdf_centroids['h3'] = gdf_centroids.apply(lat_lng_to_h3, axis=1)
+
+df_pol = gdf_centroids.groupby(['h3'],as_index=False).size().rename(columns={"size": "Aantal gebouwen"})
+df_pol['geometry'] = df_pol.apply(add_geometry, axis=1)
+df_pol = gpd.GeoDataFrame(df_pol, crs='EPSG:4326')
+
 m = tost.explore(column='Oppervlakte (m2)',legend_kwds={'interval':True},k=5,scheme='Percentiles',cmap="Reds",  name="Afmeting gebouwen",legend=False,)
 m = df_pol.explore(m=m,column='Aantal gebouwen',legend_kwds={'interval':True},k=5,scheme='Percentiles',cmap="Reds",name="Gebouwendichtheid",legend=False)
 plugins.HeatMap(heat_data,name="Heat map",min_opacity=0.5,overlay=True,show=False,
