6ceedc3a4cf00f9203c8d06860f2d39fadf11475
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-07-30 11:38:56 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,9 +8,9 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
 
   gdf_polygon = gpd.read_file(uploaded_file)
-  gdf_polygon = gdf_polygon.to_crs({'init': 'epsg:32633'})
-  gdf_polygon['Oppervlakte (Km2)'] = gdf_polygon['geometry'].map(lambda x: round(x.area / 10**6,2))
-  gdf_polygon = gdf_polygon.to_crs({'init': 'epsg:4326'})
+  # gdf_polygon = gdf_polygon.to_crs({'init': 'epsg:32633'})
+  # gdf_polygon['Oppervlakte (Km2)'] = gdf_polygon['geometry'].map(lambda x: round(x.area / 10**6,2))
+  # gdf_polygon = gdf_polygon.to_crs({'init': 'epsg:4326'})
 
 else:
 
@@ -32,17 +32,19 @@ layers = [
 INITIAL_VIEW_STATE = pdk.ViewState(
         latitude=gdf_polygon.centroid.y.mean(),
         longitude=gdf_polygon.centroid.x.mean(),
-        zoom=11,
+        zoom=8,
         pitch=0,
         bearing=0)
 
 tooltip = {
-    "text": "{naam} \nOppervlakte (Km2): {Oppervlakte (Km2)} \nAanvullende informatie: {extras}",
+    "text": "Reansect: {transect} ",
 }
 
 
-r = pdk.Deck(layers=[layers],initial_view_state=INITIAL_VIEW_STATE,tooltip=tooltip,
-              map_style=pdk.map_styles.ROAD)
+r = pdk.Deck(layers=[layers],
+             initial_view_state=INITIAL_VIEW_STATE,
+             tooltip=tooltip,
+             map_style=pdk.map_styles.ROAD)
 
 
 st.pydeck_chart(r)
